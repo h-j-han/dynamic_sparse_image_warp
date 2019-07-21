6503c48d56fd563c732be0c8c7ed0a6a1ef5bb11
comc35 <comc35@gmail.com>
2019-07-21 09:56:11 +0000

    unit test with time_warp

--- dynamic_time_warp.py
+++ dynamic_time_warp.py
@@ -11,18 +11,19 @@ def time_warp(mel_fbanks, W=80):
   
   tau, v = (tf.shape(mel_fbanks)[1], tf.shape(mel_fbanks)[2])
   rp = tf.random_uniform([], W, tau - W, tf.int32)
-  cp = tf.expand_dims(v // 2)
+  cp = tf.expand_dims(v // 2, 0)
   w = tf.random_uniform([], 0, W, tf.float32)
   
   src_ctp_loc = tf.stack((cp, tf.ones_like(cp) * rp), -1)
   src_ctp_loc = tf.to_float(tf.expand_dims(src_ctp_loc, 0))
   
-  dest_ctp_loc = tf.stack((cp, tf.ones_like(cp) * (tf.to_float(rp) + w)), -1)
+  dest_ctp_loc = tf.stack((tf.to_float(cp), 
+               tf.ones_like(cp, dtype=tf.float32) * (tf.to_float(rp) + w)), -1)
   dest_ctp_loc = tf.to_float(tf.expand_dims(dest_ctp_loc, 0))
   mel_fbanks = tf.transpoase(mel_fbanks, perm=[0, 2, 1, 3])
   warped_mel_fbanks, _ = sparse_image_warp(mel_fbanks,
                                            source_control_point_locations=src_ctp_loc,
                                            dest_control_point_locations=dest_ctp_loc,
                                            num_boundary_points=1)
-  warped_mel_fbanks = tf.transpoase(warped_mel_fbanks, perm=[0, 2, 1, 3]
+  warped_mel_fbanks = tf.transpoase(warped_mel_fbanks, perm=[0, 2, 1, 3])
   return warped_mel_fbanks
